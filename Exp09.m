bbf85861b0f1baf50c9dd7adf85994da7465767c
Shashidhar1724 <shashidhar1724@gmail.com>
2024-12-31 00:11:47 +0530

    Update Exp09.m

--- Exp09.m
+++ Exp09.m
@@ -1,35 +1,22 @@
-p = [0.4 0.3 0.2 0.1];
-n=length(p);
-symbols=[1:n];
-[dict,avglen]=huffmandict(symbols,p);
-temp=dict;
-t=dict(:,2);
-for i=1:length(temp)
-    temp{i,2}=num2str(temp{i,2});
+% Input probability distribution
+e = [1,2,4,2,3];
+p = [0.5 0.2 0.15 0.15];
+n = length(p); 
+% Generate Huffman dictionary 
+symbols = 1:n;
+[dict, avglen] = huffmandict(symbols, p);
+% Display Huffman dictionary 
+disp('The Huffman code dictionary:'); 
+for i = 1:n 
+fprintf('Symbol %d: %s\n', symbols(i), num2str(dict{i, 2})); 
 end
-disp('The huffman code dict:');
-disp(temp)
-fprintf('Enter the symbols between 1 to %d in[]',n);
-sym=input(':')
-encod=huffmanenco(sym,dict);
-disp('The encoded output:');
+% Encode symbols 
+sym = e;%input(sprintf('Enter the symbols between 1 to %d in []: ', n));
+encod = huffmanenco(sym, dict); 
+disp('The encoded output:'); 
 disp(encod);
-bits=input('Enter the bit stream in[];');
-decod=huffmandeco(bits,dict);
-disp('The symbols are:');
-disp(decod); 
-H=0;
-Z=0;
-for(k=1:n)
-    H=H+(p(k)*log2(1/p(k)));     
- end
-fprintf(1,'Entropy is %f bits',H);
-N=H/avglen;
-fprintf('\n Efficiency is:%f',N);
-for(r=1:n)
-   l(r)=length(t{r});
-end
-m=max(l)
-s=min(l)
-v=m-s;
-fprintf('the variance is:%d',v);
+% Decode bit stream 
+bits = encod; 
+decod = huffmandeco(bits, dict); 
+disp('The decoded symbols are:');
+disp(decod);
